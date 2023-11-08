f259795ea17b8f4d22e6bd59e2a3e13528f8cec5
KingDev <davidavirodrigues27@gmail.com>
2023-11-08 20:27:41 -0300

    projeto

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -18,7 +18,7 @@ class AdminController extends Controller
 
     public function loginForm()
     {
-        return view('admin.register');
+        return view('admin.login');
     }
 
     public function adminLogin(Request $request)
