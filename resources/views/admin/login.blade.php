84e3903ae17e719996213ec108238aa3fcc02e31
KingDev <davidavirodrigues27@gmail.com>
2023-11-08 20:35:56 -0300

    projeto

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -28,7 +28,7 @@
             <img src="public/associate/img/Logo Veste-me - Círculo - Preta.png" alt="">
         </div>
 
-        <form action="{{ route('admin.loginpost') }}">
+        <form action="{{ route('admin.loginpost') }}" method="post">
             <div class="row">
                 <i class="material-icons-sharp">person</i>
                 <input type="Email" placeholder="E-mail" required>
