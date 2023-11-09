55dc0f819e89a9c1fa479d079c29752b36ae71ad
KingDev <davidavirodrigues27@gmail.com>
2023-11-09 12:21:50 -0300

    projeto

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -19,7 +19,7 @@ class ImportController extends Controller
 
             foreach ($data as $row) {
                 DB::table('estilo')->insert([
-                    'estilo' => $row[2],
+                    'estilo' => $row[1],
                 ]);
             }
 
