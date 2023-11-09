3088d7304bb23b35d6083f51ba31578bf03c0186
KingDev <davidavirodrigues27@gmail.com>
2023-11-09 00:59:25 -0300

    projeto

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -18,16 +18,15 @@ class ImportController extends Controller
             $data = array_map('str_getcsv', file($path));
 
             foreach ($data as $row) {
-                DB::table('tipocorporal')->insert([
-                    'tipocorporal' => $row[1],
-                    'icone' => $row[2], // NULL ou caminho para o ícone se estiver disponível no CSV
+                DB::table('genero')->insert([
+                    'genero' => $row[1],
                 ]);
             }
 
-            return redirect()->back()->with('success', 'Dados de tipo corporal importados com sucesso.');
+            return redirect()->back()->with('success', 'Dados de gênero importados com sucesso.');
         }
     }
 
-    return redirect()->back()->with('error', 'Erro na importação de dados de tipo corporal.');
+    return redirect()->back()->with('error', 'Erro na importação de dados de gênero.');
 }
 }
