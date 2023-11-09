eea76f54c20f602aa07c1f8af2e3b002af20790d
KingDev <davidavirodrigues27@gmail.com>
2023-11-09 12:14:54 -0300

    projeto

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -18,16 +18,8 @@ class ImportController extends Controller
             $data = array_map('str_getcsv', file($path));
 
             foreach ($data as $row) {
-                DB::table('combinacao')->insert([
-                    'cod_combinacao' => $row[0],
-                    'cod_estilo' => $row[1],
-                    'cod_tipocorporal' => $row[2],
-                    'cod_ocasiao' => $row[3],
-                    'cod_Login' => $row[4],
-                    'cod_genero' => $row[5],
-                    'img_comb' => $row[6],
-                    'link_comb' => $row[7],
-                    'ocasiaoespecif_comb' => $row[8],
+                DB::table('estilo')->insert([
+                    'estilo' => $row[1],
                 ]);
             }
 
