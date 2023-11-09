d18d5f10035ebca0e3f4e4c598387da2557a8983
KingDev <davidavirodrigues27@gmail.com>
2023-11-09 00:49:24 -0300

    projeto

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -4,32 +4,30 @@ namespace App\Http\Controllers;
 
 use App\Models\Ocasiao;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ImportController extends Controller
 {
     public function importData(Request $request)
 {
-    // Verifique se um arquivo CSV foi enviado
     if ($request->hasFile('csv_file')) {
         $file = $request->file('csv_file');
 
-        // Verifique se o arquivo é válido e faça a importação
         if ($file->isValid()) {
             $path = $file->getRealPath();
             $data = array_map('str_getcsv', file($path));
 
-            // Percorra os dados e insira no banco de dados
             foreach ($data as $row) {
-                Ocasiao::create([
-                    'cod_ocasiao' => $row[0],
-                    'ocasiao' => $row[1],
+                DB::table('estado')->insert([
+                    'cod_estado' => $row[0],
+                    'estado' => $row[1],
                 ]);
             }
 
-            return redirect()->back()->with('success', 'Dados importados com sucesso.');
+            return redirect()->back()->with('success', 'Dados de estado importados com sucesso.');
         }
     }
 
-    return redirect()->back()->with('error', 'Erro na importação.');
+    return redirect()->back()->with('error', 'Erro na importação de dados de estado.');
 }
 }
