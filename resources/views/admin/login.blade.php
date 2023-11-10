109e73631a20b10a11c4645b7e3530a874f76487
KingDev <davidavirodrigues27@gmail.com>
2023-11-10 04:56:31 -0300

    projeto

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -7,6 +7,7 @@
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
    <!-- CSS -->
    <link rel="stylesheet" href="public/associate/css/login.css">
